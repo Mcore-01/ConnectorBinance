6669b060694e346c09520e64b1b5b23e9a30e5e0
Mcore-01 <dassd2123@gmail.com>
2025-03-13 17:15:00 +0500

    Создал UI WPF, добавил визуализацию трейдов

---
+++ ConnectorBinanceUI/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ConnectorBinanceUI.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectorBinanceUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TradeViewModel tradeViewModel = new TradeViewModel();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = tradeViewModel;
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out _);
+        }
+    }
+}
---
+++ ConnectorBinanceUI/ViewModel/TradeViewModel.cs
@@ -0,0 +1,69 @@
+﻿using ConnectorBinanceUI.Commands;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using TestTrade;
+using TestTrade.Models;
+
+namespace ConnectorBinanceUI.ViewModel;
+
+public class TradeViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<Trade> Trades { get; set; } = new ObservableCollection<Trade>();
+   
+    private ConnectorBinance _connectorBinance;
+
+    private string pair = "BTCUSDT";
+    public string Pair
+    {
+        get { return pair; }
+        set 
+        { 
+            pair = value;
+            OnPropertyChanged("Pair");
+        }
+    }
+
+    private int maxCount = 10;
+    public int MaxCount
+    {
+        get { return maxCount; }
+        set 
+        { 
+            maxCount = value;
+            OnPropertyChanged("MaxCount");
+        }
+    }
+
+
+    private RelayCommand getTrades;
+    public RelayCommand GetTrades
+    {
+        get
+        {
+            return getTrades ??
+              (getTrades = new RelayCommand(async obj =>
+              {
+                  var trades = await  _connectorBinance.GetNewTradesAsync(Pair, MaxCount);
+
+                  Trades.Clear();
+
+                  foreach (Trade trade in trades)
+                  {
+                      Trades.Add(trade);
+                  }
+              }));
+        }
+    }
+
+    public TradeViewModel()
+    {
+        _connectorBinance = new ConnectorBinance();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
